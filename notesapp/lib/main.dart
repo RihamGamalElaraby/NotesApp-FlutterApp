b5753614f01f443897c4d27fdde59ddc6a29a8c7
RehamElaraby <rehamelaraby2272@gmail.com>
2024-04-14 23:39:03 +0200

    add  UI

---
+++ notesapp/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import 'views/notesVeiw.dart';
+
+void main() {
+  runApp(const NotesApp());
+}
+
+class NotesApp extends StatelessWidget {
+  const NotesApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'NotesApp',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        fontFamily: 'Poppins',
+      ),
+      home: const NotesView(),
+    );
+  }
+}
---
+++ notesapp/lib/widgets/CustomNoteItem.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(top: 14, bottom: 14, left: 14),
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            title: Text(
+              'Flutter Tips',
+              style: TextStyle(color: Colors.black, fontSize: 26),
+            ),
+            subtitle: Padding(
+              padding: const EdgeInsets.only(top: 16),
+              child: Text('Flutter fsdgfgfffffffffffffffffffffff',
+                  style: TextStyle(
+                      color: Colors.black.withOpacity(0.4), fontSize: 20)),
+            ),
+            trailing: IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.delete,
+                  color: Colors.black,
+                  size: 35,
+                )),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 24, top: 16, bottom: 16),
+            child: Text(
+              'May1 , 2024',
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.black.withOpacity(0.4),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
